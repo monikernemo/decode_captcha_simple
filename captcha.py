584e595eb08553b4401b3063468b9b21be481032
monikernemo <leeyiquan95@gmail.com>
2022-08-24 03:23:47 +0800

    include some comments

--- captcha.py
+++ captcha.py
@@ -1,5 +1,20 @@
 from knn_model import knn_model
 
+'''
+Strategy: 
+For ease of calculation we normalize 0-255 to floats 0-1. 
+Remove noise by letting slightly pixels with sum > 0.5 go to 1 (sum up all RGB coordinates), otherwise set them to 0.
+
+Since characters are evenly space, an easy data exploration reviews that each space is 1 pixel wide, 
+each character is 7 pixel wide, characters starts from  5-th pixel from the left (if zero index).
+
+Height-wise, characters start from 11-th pixel from the top; height of characters are 9 pixels tall, so 63 pixels per char.
+
+Each captcha, first segment into characters, and compare to dictionary of characters we have for each character encountered in sample data.
+Find closest match in terms of L1 - distance. (arguably, can choose other metrics such as Frobenius norm so less sensitive to noise). 
+
+After matching all characters, return captcha phrase by concatenating all characters.
+'''
 class Captcha(object):
     def __init__(self):
         self.model = knn_model()
